1ef1f34bacd76576908b625134792913a4c69e12
msyksphinz-self <msyksphinz.dev@gmail.com>
2025-11-29 14:06:24 +0000

    Update for qemu execution

--- linux_on_qemu/qemu_riscv_boot.sh
+++ linux_on_qemu/qemu_riscv_boot.sh
@@ -1,6 +1,6 @@
 qemu-system-riscv64 -nographic -machine virt \
                     -kernel /linux_riscv/linux/arch/riscv/boot/Image \
                     -initrd /linux_riscv/rootfs.cpio \
-                    -drive file=/linux_riscv/testfs.img,format=raw,id=testfs \
+                    -drive file=/linux_riscv/testfs.img,format=raw,id=testfs,if=none \
                     -device virtio-blk-device,drive=testfs \
                     -append "console=ttyS0 rdinit=/init"
